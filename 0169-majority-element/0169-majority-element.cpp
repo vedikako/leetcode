f3c7c356d2d7c303d1ca3a77e0ade31ae7879448
Vedika Kodgire <vedika.kodgire12@gmail.com>
2025-10-14 20:04:16 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.67%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map <int,int> mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]]++;
+        }
+        for (auto it: mpp){
+            if (it.second>(nums.size()/2)){
+                return it.first;
+            }
+        }return -1;
+
+    }
+};
