412ccd0498e3cdbcbaa37a5d7c84ebcea6a9e1c4
Vedika Kodgire <vedika.kodgire12@gmail.com>
2026-06-01 14:39:56 +0530

    Time: 3 ms (62.15%), Space: 8.4 MB (11.66%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,26 @@
 class Solution {
 public:
-    int fib(int n) {
-        if( n==0 || n==1) return n;
-        return fib(n-1)+fib(n-2);
 
+    // memoisation code
+    int mem(int i, vector<int>& dp) {
+        if (i == 0 || i == 1)
+            return i;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        return dp[i] = mem(i - 1, dp) + mem(i - 2, dp);
     }
 
+    int fib(int n) {
+        // return rec(n);
+
+        vector<int> dp(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            dp[i] = -1;
+        }
+
+        return mem(n, dp);
+    }
 };
