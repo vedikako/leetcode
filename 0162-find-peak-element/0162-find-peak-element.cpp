12ed1f82833811db8506cba031285cc4998a4d8c
Vedika Kodgire <vedika.kodgire12@gmail.com>
2026-06-05 13:49:29 +0530

    Time: 0 ms (100%), Space: 12.7 MB (5.45%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,56 +1,10 @@
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-//         int l = 0;
-//         int n = nums.size();
-//         int h = n - 1;
-//         int ans = -1;
-//         if(n==1)return 0;
-//         if(n==2 && nums[l]>nums[h])return l;
-//         if(n==2 && nums[l]<nums[h])return h;
-//         while (l < h) {
-//             int m = l + (h - l) / 2;
-
-//             if (nums[m] > nums[m - 1] && nums[m] > nums[m + 1])
-//                 return m;   
-//             else {
-//                 l = m + 1;
-//                 ans = m;
-//             }
-//             else{
-//                 h=m-1;
-//             } 
-//         }
-
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
+        auto max_it = max_element(nums.begin(), nums.end());
 
-        if(n == 1) return 0;
-
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
-
-        int l = 1;
-        int h = n - 2;
-
-        while(l <= h){
-            int m = l + (h-l)/2;
-
-            if(nums[m] > nums[m-1] && nums[m] > nums[m+1])
-                return m;
-
-            else if(nums[m] < nums[m+1])
-                l = m + 1;
-
-            else
-                h = m - 1;
-        }
-
-        return -1;
+    // 3. Calculate the index from the iterator
+         int max_idx = distance(nums.begin(), max_it);
+         return max_idx;
     }
 };
