bfb995c02c1dbe9aa972dd2d57cb6e7b7bd134d5
Vedika Kodgire <vedika.kodgire12@gmail.com>
2026-06-09 20:46:05 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,28 +1,13 @@
-// class Solution {
-// public:
-//     int climbStairs(int n) {
-//         if (n==1) return 1;
-//         if (n==2)return 2;
-
-//         return climbStairs(n-1)+climbStairs(n-2);
-//     }
-// };
-
 class Solution {
 public:
-    int solve(int n, vector<int>& dp)
-    {
-        if(n <= 1)
-            return 1;
-
-        if(dp[n] != -1)
-            return dp[n];
-
-        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
-    }
-
     int climbStairs(int n) {
-        vector<int> dp(n + 1, -1);
-        return solve(n, dp);
+        if(n==1) return 1;
+        int dp [n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for (int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
